b92806a7798423645e5044f0b5615824969d2903
Dixzz <vishusatyam36@gmail.com>
2021-02-16 16:47:05 +0530

    Link pages

--- colos/urls.py
+++ colos/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('blog', views.blog, name='blog'),
+    path('contact', views.contact, name='contact'),
+    path('gallery', views.gallery, name='gallery'),
+    path('pricing', views.pricing, name='pricing'),
+    path('schedule', views.schedule, name='schedule'),
+    path('single_post', views.about, name='single_post'),
+    path('speakers', views.speakers, name='speakers'),
+    path('sponsors', views.about, name='sponsors'),
 ]
--- colos/views.py
+++ colos/views.py
@@ -1,4 +1,41 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+
+def pricing(request):
+    return render(request, 'pricing.html')
+
+
+def schedule(request):
+    return render(request, 'schedule.html')
+
+
+def single_post(request):
+    return render(request, 'single-post.html')
+
+
+def speakers(request):
+    return render(request, 'speakers.html')
+
+
+def sponsors(request):
+    return render(request, 'sponsors.html')
